56f137fc9f2e9d34742f8f96ee5402e2090aab5f
hjain0726 <hjain0726@gmail.com>
2020-01-28 22:46:56 +0530

    file added by harsh

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
-var player={
-  __score:0,
-  __live:3,
-  __total:30
+var player = {
+  __score: 0,
+  __live: 3,
+  __total: 30
 }
 
-String.prototype.replaceAt=function(index, replacement) {
-  return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substring(0, index) + replacement + this.substring((index+1) ,this.length+1);
 }
 
-function start(){
-     var name = document.getElementById('name').value
-     var age = document.getElementById('age').value
-     var level = document.getElementById('level').value;
-     document.getElementById("info").style.display = "none";
-     document.getElementById(`${level}`).style.display = "block";
+function start() {
+  var name = document.getElementById('name').value
+  var age = document.getElementById('age').value
+  var level = document.getElementById('level').value;
+  document.getElementById("info").style.display = "none";
+  document.getElementById(`${level}`).style.display = "block";
 }
-    function hit(div_id,ans_id,ch){
-        var div=" ",arr=" "; 
-        div = document.getElementById(div_id).innerHTML;
-        var ans = document.getElementById(ans_id).innerHTML;
-        var indx=[];
-        if(player.live>0){
-          if(ans.indexOf(ch)>-1){
-        for (let i in ans){
-          if(ans[i]==ch){
-           indx.push(i);
-          }
-        }
-        }else{
-          player.__live-=1;
-          console.log(player.__live)
-        }   
-        for (let n in indx){
-          arr = div.replaceAt(indx[n],ch)
-         document.getElementById(div_id).innerHTML = arr;
+
+
+function hit(div_id, ans_id, ch) {
+  var div = " ", arr = " ";
+  div = document.getElementById(div_id).innerHTML;
+  var ans = document.getElementById(ans_id).innerHTML;
+  ans = ans.split("").join(" ");
+
+  var indx = [];
+
+    if (player.__live > 0 &&  ans.localeCompare(document.getElementById(div_id).innerHTML)!=0) {
+    if (ans.indexOf(ch) > -1) {
+      for (let i in ans) {
+        if (ans[i] == ch) {
+          indx.push(i);
         }
       }
-      // if(arr.localeCompare(div)==0){
-      //   player.__score+=10;
-      //   console.log(player.__score)
-      // }
-  }
+    } else {
+      player.__live -= 1;
+      console.log("lost")
+    }
+  
+    for (let n in indx) {
 
+      arr = document.getElementById(div_id).innerHTML.split("");
+      arr[indx[n]]=ch;
+
+      arr = arr.join("")
+      document.getElementById(div_id).innerHTML = arr;
+
+    }
+    if(arr.localeCompare(ans)==0){
+      player.__score+=10;
+      console.log(player.__score)
+   
+  }
+  }
+ 
+}
