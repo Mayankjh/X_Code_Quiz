c5470375328f1d648fc2b11753c4880d8de82c60
mayank <rahulsharma25252599@gmail.com>
2020-01-29 00:55:14 +0530

    add

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ var player = {
   __total: 30
 }
 
-String.prototype.replaceAt = function (index, replacement) {
-  return this.substring(0, index) + replacement + this.substring((index+1) ,this.length+1);
+function check(){
+if(document.getElementById('name').value == "" && document.getElementById('age').value=="" ){
+  document.getElementById("start").disabled = true;
+}else{
+  document.getElementById("start").disabled = false;
+}
 }
 
-function start() {
-  var name = document.getElementById('name').value
+setInterval(check,2000);
+
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substring(0, index) + replacement + this.substring((index + 1), this.length + 1);
+}
+
+function quit(){
+  document.getElementById(document.getElementById('level').value).style.display = "none";
+  document.getElementById('quit').style.display = "block";
+  document.getElementById("score").innerHTML=player.__score;
+
+}
+
+function start(){
+  var name = document.getElementById('name').value;
   var age = document.getElementById('age').value
   var level = document.getElementById('level').value;
+  document.getElementById('hi').innerHTML=name;
   document.getElementById("info").style.display = "none";
   document.getElementById(`${level}`).style.display = "block";
+  console.log(document.getElementById(`${level}`).style.display)
 }
 
 
@@ -25,7 +44,7 @@ function hit(div_id, ans_id, ch) {
 
   var indx = [];
 
-    if (player.__live > 0 &&  ans.localeCompare(document.getElementById(div_id).innerHTML)!=0) {
+  if (player.__live > 0 && ans.localeCompare(document.getElementById(div_id).innerHTML) != 0) {
     if (ans.indexOf(ch) > -1) {
       for (let i in ans) {
         if (ans[i] == ch) {
@@ -36,21 +55,21 @@ function hit(div_id, ans_id, ch) {
       player.__live -= 1;
       console.log("lost")
     }
-  
+
     for (let n in indx) {
 
       arr = document.getElementById(div_id).innerHTML.split("");
-      arr[indx[n]]=ch;
+      arr[indx[n]] = ch;
 
       arr = arr.join("")
       document.getElementById(div_id).innerHTML = arr;
 
     }
-    if(arr.localeCompare(ans)==0){
-      player.__score+=10;
+    if (arr.localeCompare(ans) == 0) {
+      player.__score += 10;
       console.log(player.__score)
-   
+
+    }
   }
-  }
- 
+
 }
